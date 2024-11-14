11bf4dd3d2da08a2541dcd2500bb4fec08501d55
erofeeva-plastilin <erofeeva@plastilin.team>
2024-11-14 14:08:50 +0300

    Update chain_files.py

--- CoordTransfer/CoordTransfer/chain_files.py
+++ CoordTransfer/CoordTransfer/chain_files.py
@@ -1,11 +1,12 @@
 import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CHAIN_DIR = os.path.join(BASE_DIR, "../Chain_files")
 CHAIN_FILES = {
     'soybean_a1v1': os.path.join(CHAIN_DIR, 'Glycine_max_a1.v1.fasta.to.GCF_000004515.6_Glycine_max_v4.0_genomic.unmasked.fna.over.chain'),
     'soybean_a2v1': os.path.join(CHAIN_DIR, 'Glycine_max_a2.v1.fasta.to.GCF_000004515.6_Glycine_max_v4.0_genomic.unmasked.fna.over.chain'),
     'refbeet1.2.2': os.path.join(CHAIN_DIR, 'GCF_000511025.2_RefBeet-1.2.2_genomic.fna.to.GCF_026745355.1_EL10.2_genomic.unmasked.fna.over.chain')
 }
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CHAIN_DIR = os.path.join(BASE_DIR, "../Chain_files")
+
 
 def get_chain_file(assembly_name):
     chain_file = CHAIN_FILES.get(assembly_name)
