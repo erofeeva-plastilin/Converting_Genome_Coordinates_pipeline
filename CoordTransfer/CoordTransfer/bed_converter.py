df8f614388dd8e4d033041f392e54f3e5e33e96d
erofeeva-plastilin <erofeeva@plastilin.team>
2024-11-15 15:23:35 +0300

    Update bed_converter.py

--- CoordTransfer/CoordTransfer/bed_converter.py
+++ CoordTransfer/CoordTransfer/bed_converter.py
@@ -1,4 +1,4 @@
 def create_bed_file(input_data, output_bed_file):
     with open(output_bed_file, 'w') as bed_file:
         for _, row in input_data.iterrows():
-            bed_file.write(f"{row['chr_original']}\t{row['pos_original']}\t{row['pos_original_end']}\n")
+            bed_file.write(f"{row['chr_original']}\t{row['pos_start_original']}\t{row['pos_end_original']}\n")
